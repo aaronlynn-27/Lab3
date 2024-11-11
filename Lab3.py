dc37d3c054afa8485588fc2bc01a624033ceaeb6
aaronlynn <lynnthitkhant27@gmail.com>
2024-11-11 18:20:05 +0800

    New test functions

--- Lab3.py
+++ Lab3.py
@@ -12,6 +12,19 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
+    # Return 0 if no numbers are entered (REQ 4)
+    if n == 0:
+        return 0  
+    
+    # Return 1 if 10 or more numbers are entered (REQ 3)
+    elif n >= 10:
+        return 1  
+
+    # Return 2 if any element is not an integer (REQ 5)
+    for x in arr:
+        if not isinstance(x, int):
+            return 2   
+
     if n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
@@ -41,8 +54,11 @@ def bubble_sort(arr, sorting_order):
 def main():
     # Driver code to test above
     arr = [64, 34, 25, 12, 22, 11, 90]
+    arr_2 = [64, 34, 25, 12, 22, 11, 90, 64, 34, 25, 12, 22, 11, 90]
+    arr_3 = []
+    arr_4 = ['ET0735', 1, 2, 3, 4, 5, 'DevOPs']
 
-    # Sort in ascending order
+     # Sort in ascending order
     result = bubble_sort(arr, SORT_ASCENDING)
     print("\nSorted array in ascending order: ")
     print(result)
@@ -50,6 +66,21 @@ def main():
     # Sort in descending order
     print("Sorted array in descending order: ")
     result = bubble_sort(arr, SORT_DESCENDING)
+    print(result)
+
+    # If >= 10 numbers are entered
+    print("If >= 10 numbers are entered ")
+    result = bubble_sort(arr_2, SORT_ASCENDING)
+    print(result)
+
+    # If 0 numbers are entered
+    print("If 0 numbers are entered")
+    result = bubble_sort(arr_3, SORT_ASCENDING)
+    print(result)
+
+    # If any of the values entered are not integers 
+    print("If any of the value entered are not integers ")
+    result = bubble_sort(arr_4, SORT_ASCENDING)
     print(result)
 
 if __name__ == "__main__":
--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,27 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_more_than_10():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 12, 23, 53, 63]
+    test = 1
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_DESCENDING)
+
+    assert (result == test)
+
+def test_bubble_no_number():
+    result = []
+    input_arr = []
+    test = 0
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_DESCENDING)
+
+    assert(result == test)
+
+def test_bubble_no_integer():
+    result = []
+    input_arr = [22,14,"sg lee pl",1]
+    test = 2
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_DESCENDING)
+
+    assert(result == test)
